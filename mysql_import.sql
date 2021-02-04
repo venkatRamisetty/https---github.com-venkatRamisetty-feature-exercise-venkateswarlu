bb8e06dfcefec779e89248d6445608fa758426c4
venkat ramisetty <venki.ramisetty66@gmail.com>
2021-02-04 09:21:29 +0530

    code
    code added

---
+++ mysql_import.sql
@@ -0,0 +1,22 @@
+
+CREATE TABLE IF NOT EXISTS `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_code` varchar(60) NOT NULL,
+  `product_name` varchar(60) NOT NULL,
+  `product_desc` tinytext NOT NULL,
+  `product_img_name` varchar(60) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `product_code` (`product_code`)
+) AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `product_code`, `product_name`, `product_desc`, `product_img_name`, `price`) VALUES
+(1, 'A', 'A', 'Di sertakan secara rambang yang lansung tidak munasabah. Jika anda ingin menggunakan Lorem Ipsum, anda perlu memastikan bahwa tiada apa yang', 'android-phone.jpg', 50),
+(2, 'B', 'B', 'Ia menggunakan kamus yang mengandungi lebih 200 ayat Latin, bersama model dan struktur ayat Latin, untuk menghasilkan Lorem Ipsum yang munasabah.', 'lcd-tv.jpg', 30),
+(3, 'C', 'C Hard Disk', 'Ada banyak versi dari mukasurat-mukasurat Lorem Ipsum yang sedia ada, tetapi kebanyakkannya telah diubahsuai, lawak jenaka diselitkan, atau ayat ayat yang', 'external-hard-disk.jpg', 20),
+(4, 'D', 'D Watch GE2', 'Memalukan akan terselit didalam di tengah tengah kandungan text. Semua injin Lorem Ipsum didalam Internet hanya mengulangi text, sekaligus menjadikan injin kami sebagai yang terunggul dan tepat sekali di Internet.', 'wrist-watch.jpg', 15),
+(5, 'E', 'E Watch GE2', 'Memalukan akan terselit didalam di tengah tengah kandungan text. Semua injin Lorem Ipsum didalam Internet hanya mengulangi text, sekaligus menjadikan injin kami sebagai yang terunggul dan tepat sekali di Internet.', 'wrist-watch.jpg', 15);
---
+++ view_cart.php
@@ -0,0 +1,110 @@
+<?php
+session_start();
+include_once("config.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>View shopping cart</title>
+<link href="style/style.css" rel="stylesheet" type="text/css"></head>
+<body>
+<h1 align="center">View Cart</h1>
+<div class="cart-view-table-back">
+<form method="post" action="cart_update.php">
+<table width="100%"  cellpadding="6" cellspacing="0"><thead><tr><th>Quantity</th><th>Name</th><th>Price</th><th>Total</th><th>Remove</th></tr></thead>
+  <tbody>
+ 	<?php
+	if(isset($_SESSION["cart_products"])) //check session var
+    {
+		$total = 0; //set initial total value
+		$b = 0; //var for zebra stripe table 
+		foreach ($_SESSION["cart_products"] as $cart_itm)
+        {
+			//set variables to use in content below
+			$product_name = $cart_itm["product_name"];
+			$product_qty = $cart_itm["product_qty"];
+			$product_price = $cart_itm["product_price"];
+			$product_code = $cart_itm["product_code"];
+			$product_color = $cart_itm["product_color"];
+
+// special discount price
+$rebatesA = array( 1 => 50, 3 => 130);  
+$rebatesB = array( 1 => 30, 2 => 45);  
+$rebatesC = array( 1 => 20, 2 => 38, 3 => 50);  
+$rebatesD = array( 1 => 15, 5 => 130);  
+$rebatesE = array( 1 => 5); 
+
+
+$count = $product_qty;
+$item=$product_code;
+//$Aitemcount=$_REQUEST["aitem"];
+$Aprice=$Bprice=$Cprice=$Dprice=$Eprice=0;
+
+if ($item == 'A'){	
+
+		$discount=$rebatesA[3]/3;
+	    if($count <= 2){ $Aprice=  $count * $rebatesA[1]; }
+	    if($count >=3){ $Aprice= round($discount * $count);}  
+     $subtotal= $Aprice ;
+	 $Aitemcount=$count;
+}
+
+if ($item == 'B'){
+		$discount=$rebatesB[2]/2;
+	    if($count==1){ $Bprice= $rebatesB[1]; }
+	    if($count >=2){ $Bprice= round($discount * $count);} 
+      $subtotal= $Bprice;
+	   }
+
+if ($item == 'C'){	
+	$discount=$rebatesC[3]/3;
+	if($count==1){ $Cprice= $rebatesC[1]; }
+	if($count==2){ $Cprice= $rebatesC[2];}
+	if($count >=3){ $Cprice= round($discount * $count);}  
+  $subtotal=$Cprice;
+}
+
+if ($item == 'D'){	
+//echo $count;
+$Aitemcount=isset($Aitemcount)? isset($Aitemcount):'';
+    if($Aitemcount > $count){$Dcount=$Aitemcount-$count;}else{ $Dcount=$count-$Aitemcount;}	
+	if($count){ $Dprice= $Aitemcount*5 + $Dcount*15; }  
+    $subtotal=$Dprice;
+}
+
+if ($item == 'E'){			
+	$subtotal= $count*$rebatesE[1];   
+  //  echo $Eprice;
+}
+
+			
+		   	$bg_color = ($b++%2==1) ? 'odd' : 'even'; 
+		    echo '<tr class="'.$bg_color.'">';
+			echo '<td><input type="text" size="2" maxlength="2" name="product_qty['.$product_code.']" value="'.$product_qty.'" /></td>';
+			echo '<td>'.$product_name.'</td>';
+			echo '<td>'.$currency.$product_price.'</td>';
+			echo '<td>'.$currency.$subtotal.'</td>';
+			echo '<td><input type="checkbox" name="remove_code[]" value="'.$product_code.'" /></td>';
+            echo '</tr>';
+			$total = ($total + $subtotal); //add subtotal to total var
+        }
+		
+		$grand_total = $total ; //grand total including shipping cost
+		
+		
+		
+	}
+    ?>
+    <tr><td colspan="5"><span style="float:right;text-align: right;">Amount Payable : <?php echo sprintf("%01.2f", $grand_total);?></span></td></tr>
+    <tr><td colspan="5"><a href="index.php" class="button">Add More Items</a><button type="submit">Update</button></td></tr>
+  </tbody>
+</table>
+<input type="hidden" name="return_url" value="<?php 
+$current_url = urlencode($url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+echo $current_url; ?>" />
+</form>
+</div>
+
+</body>
+</html>
